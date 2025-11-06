de07afd1f096a19d2810b34ba243f2ac1ec3581a
daniel.sardina <daniel.sardina@educa.madrid.org>
2025-11-06 23:28:25 +0100

    Creacion de tabla usuarios

--- init.sql
+++ init.sql
@@ -1,6 +1,9 @@
-CREATE TABLE IF NOT EXISTS test (
-    id INT PRIMARY KEY
+CREATE TABLE IF NOT EXISTS users (
+    email VARCHAR(255) PRIMARY KEY, 
+    name VARCHAR(100) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    balance DECIMAL(10,2) DEFAULT 0.00
 );
 
-INSERT INTO test (id)
-VALUES (1);
+INSERT INTO users (email, name, password, balance)
+VALUES ('pepe@mail.com', 'Pepe', '1234', 1500.00);
