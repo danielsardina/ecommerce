eeec9bee459d06ca890bb729e21a450793938f34
danielsardina <daniel.sardina@educa.madrid.org>
2025-11-13 19:51:24 +0100

    Añadida tabla productos

--- init.sql
+++ init.sql
@@ -5,5 +5,16 @@ CREATE TABLE IF NOT EXISTS users (
     balance DECIMAL(10,2) DEFAULT 0.00
 );
 
-INSERT INTO users (email, name, password, balance)
-VALUES ('pepe@mail.com', 'Pepe', '1234', 1500.00);
+CREATE TABLE IF NOT EXISTS products (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    price DECIMAL(10,2) DEFAULT 0.00,
+    stock INT DEFAULT 0
+);
+
+INSERT INTO products (name, price, stock)
+VALUES 
+    ("salmon", 12.99, 8),
+    ("bacalao", 8.55, 23),
+    ("merluza", 6.75, 31),
+    ("rape", 9.99, 3);
