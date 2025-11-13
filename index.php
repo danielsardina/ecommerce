4cad52ae53699e886cd698fc62ec160c9480832c
FabiSal <fabilucas97@gmail.com>
2025-11-13 23:17:59 +0100

    Añadido productos al index.php

--- index.php
+++ index.php
@@ -1,18 +1,41 @@
+<?php require_once("bd.php") ?>
 <?php require_once("auth_required.php") ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
     <title>Document</title>
 </head>
+
 <body>
     <?php include_once("header.php") ?>
     <main>
         <h1>SS Pescados</h1>
+
+        <div>
+            <?php
+            $stmt = $mysqli->prepare("SELECT * FROM products ");
+            $stmt->execute();
+            $resultado = $stmt->get_result();
+
+            if ($resultado->num_rows > 0) {
+                $productos = $resultado->fetch_all();
+                foreach ($productos as $producto) {
+                    echo "nombre: ".$producto[1]." - Precio: ".$producto[2]."<br>";
+                }
+            }
+
+            ?>
+        </div>
+
+
+
     </main>
     <?php include_once("footer.php") ?>
 </body>
+
 </html>
