319c73a19624d0b9b9c038584e6177e3d798ef7d
danielsardina <daniel.sardina@educa.madrid.org>
2025-11-14 17:45:46 +0100

    nombre y saldo de usuario en el header

--- header.php
+++ header.php
@@ -1,7 +1,20 @@
+<?php
+if (isset($_SESSION["email"])) {
+    $stmt = $mysqli->prepare("SELECT * FROM users WHERE email = ?");
+    $stmt->bind_param("s", $_SESSION["email"]);
+    $stmt->execute();
+    $resultado = $stmt->get_result();
+    $user = $resultado->fetch_assoc();
+}
+?>
+
 <header>
     <nav>
         <a href="login.php">Login</a>
         <a href="register.php">Register</a>
         <a href="logout.php">Logout</a>
     </nav>
+    <div>
+        <?= "<p>" . $user['name'] . " : <span>" . $user["balance"] . "</span></p>" ?>
+    </div>
 </header>
